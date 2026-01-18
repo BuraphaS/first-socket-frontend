5f9bf416f86946dac1052463a452e46ae5c9d154
Burapha.s <burapha.s2545@gmail.com>
2026-01-18 08:53:05 +0700

    cors

--- src/utils/Socket.ts
+++ src/utils/Socket.ts
@@ -1,4 +1,4 @@
 import { io, type Socket } from 'socket.io-client'
 
-export const bingoSocket: Socket = io(`${import.meta.env.VITE_APP_API_URL}/bingo`,)
-export const chatSocket: Socket = io(`${import.meta.env.VITE_APP_API_URL}/chat`,)
+export const bingoSocket: Socket = io(`${import.meta.env.VITE_APP_API_URL}`,{ transports: ['websocket'] })
+export const chatSocket: Socket = io(`${import.meta.env.VITE_APP_API_URL}`,{ transports: ['websocket'] })
